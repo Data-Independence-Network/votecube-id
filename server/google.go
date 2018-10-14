b96f392f7201ad12247f1d5c8b0977ba6b424ddb
russoturisto <russoturisto@gmail.com>
2018-10-14 16:28:44 -0600

    added example

---
+++ server/google.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
+type MyHandler struct {
+	foobar string
+}
+
+// request handler in net/http style, i.e. method bound to MyHandler struct
+func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, world! Requested path is %q. Foobar is %q",
+		ctx.Path(), h.foobar)
+}
+
+func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hi there! Request URI is %q", ctx.RequestURI())
+}
+
+myHandler := &MyHandler {
+	foobar: "foobar"
+}
+
+fasthttp.ListenAndServe(":8080", myHandler.HandleFastHTTP)
+
+// pass plain function to fasthttp
+fasthttp.ListenAndServe(":8081", fastHTTPHandler)
